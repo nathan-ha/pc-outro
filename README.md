5a85f961464b538bd77ffe076216a2940045cce2
Nathan Ha <nathan_ha@outlook.com>
2023-07-22 16:52:25 -0700

    wrote the program

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pc-outro
+# makes your pc shut off with some flare
+
+Usage: just run the respective .py file
+
+- Make sure to disable hibernate for shutdown.py to work. To disable hibernate, use 'Powercfg -H OFF' using the terminal.
+- If you want to change the song or picture, just replace the stuff in the media folder (keep same file names if you don't want to edit code)
+- If the timing is off, then you have to change the offset in the code
---
+++ sleep.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+# both constants are in seconds
+SONG_START_TIME = 14.88
+SONG_END_TIME = 28 # technically it should be ~30, but the offset accounts for it
+OFFSET = 0.18
+
+os.startfile('media\outro.mp3')
+time.sleep(SONG_START_TIME)
+os.startfile('media\ded.jpg')
+
+print('Your PC is gonna die in:')
+time.sleep(OFFSET)
+
+wait_time = round(SONG_END_TIME - SONG_START_TIME)
+for i in range(wait_time):
+    time.sleep(1)
+    print(wait_time - i)
+
+print("\ngoodnight")
+os.system('rundll32.exe powrprof.dll,SetSuspendState 0,1,0') # puts the pc to sleep
